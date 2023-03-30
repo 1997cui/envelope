4abce52312478214739728b86d13fe6be7d8752c
Tianyi Cui <1997cui@gmail.com>
2023-03-29 22:37:45 -0700

    Split address to multiple line, serial number is generated

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask
+from . import config
 app = Flask(__name__)
+app.secret_key = config.FLASK_SESSION_KEY
 
 from . import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template, request, make_response, jsonify
+from flask import Flask, render_template, request, make_response, jsonify, session
 from . import imb
 from . import config
 import pdfkit
 from . import usps_api
 from . import app
+import datetime
+import redis
+
+ROLLING_WINDOW = 50
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+def generate_serial():
+    today = datetime.datetime.today()
+    num_days = (today - datetime.datetime(1970,1,1)).days
+    base = num_days % ROLLING_WINDOW
+    day_key = "serial_" + str(base)
+    perday_counter = redis_client.incr(day_key)
+    if perday_counter >= 9999:
+        raise ValueError
+    redis_client.expire(day_key, 48 * 60 * 60)
+    return base * 10000 + int(perday_counter)
 
 def generate_human_readable(receipt_zip: int, serial: int):
     return "{0:02d}-{1:03d}-{2:d}-{3:06d}-{4:d}".format(config.BARCODE_ID, config.SRV_TYPE, config.MAILER_ID, serial, receipt_zip)
@@ -20,19 +36,44 @@ def index():
 @app.route('/generate', methods=['POST'])
 def generate():
     sender_address = request.form['sender_address']
-    recipient_address = request.form['recipient_address']
+    recipient_name = request.form['recipient_name']
+    recipient_company = request.form.get('recipient_company', '')
+    recipient_street = request.form['recipient_street']
+    recipient_address2 = request.form.get('recipient_address2', '')
+    recipient_city = request.form['recipient_city']
+    recipient_state = request.form['recipient_state']
     try:
-        receipt_zip = int(request.form['receipt_zip'])
-        serial = int(request.form['serial'])
+        recipient_zip = int(request.form['recipient_zip'])
     except:
         response = "Serial number or receipt zip is not number!"
         return response
+    recipient_address_parts = [
+        recipient_name,
+        recipient_company,
+        recipient_street,
+        recipient_address2,
+        f"{recipient_city}, {recipient_state}, {request.form['recipient_zip']}"
+    ]
+    recipient_address = '\n'.join(filter(bool, recipient_address_parts))
+    serial = generate_serial()
+    session['sender_address'] = sender_address
+    session['recipient_address'] = recipient_address
+    session['serial'] = serial
+    session['recipient_zip'] = recipient_zip
+    return render_template('generate.html', serial=serial, recipient_zip=recipient_zip)
+
+@app.route('/download/<type>')
+def download(type: str):
+    sender_address = session['sender_address']
+    recipient_address = session['recipient_address']
+    serial = session['serial']
+    recipient_zip = session['recipient_zip']
     html = render_template('envelopepdf.html', sender_address=sender_address, recipient_address=recipient_address, 
-                           human_readable_bar=generate_human_readable(receipt_zip, serial), 
-                           barcode=imb.encode(config.BARCODE_ID, config.SRV_TYPE, config.MAILER_ID, serial, str(receipt_zip)))
-    if 'htmlgenerate' in request.form:
+                           human_readable_bar=generate_human_readable(recipient_zip, serial), 
+                           barcode=imb.encode(config.BARCODE_ID, config.SRV_TYPE, config.MAILER_ID, serial, str(recipient_zip)))
+    if type == 'html':
         return html
-    if 'pdfgenerate' in request.form:
+    elif type == 'pdf':
         options={
             'page-height': '4.125in',
             'page-width': '9.5in',
@@ -47,7 +88,8 @@ def generate():
         response.headers['Content-Type'] = 'application/pdf'
         response.headers['Content-Disposition'] = 'attachment; filename=envelope.pdf'
         return response
-    return "Empty"
+    else:
+        return "Type not valid"
 
 @app.route('/track', methods=['POST'])
 def track():
