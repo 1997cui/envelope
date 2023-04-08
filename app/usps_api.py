c68487e4f4f58ee39e3a70945c516dcf381bbbbe
Tianyi Cui <1997cui@gmail.com>
2023-04-07 21:58:17 -0700

    Add error handling code

--- app/usps_api.py
+++ app/usps_api.py
@@ -1,16 +1,18 @@
-from . import config
-import aioredis
+from urllib.parse import urljoin
+import asyncio
 import datetime
 import time
 import sys
-import xmltodict
 import html
+import xmltodict
+import aioredis
 import httpx
-import asyncio
-from urllib.parse import urljoin
 
-USPS_API_URL="https://services.usps.com"
-USPS_SERVICE_API_BASE="https://iv.usps.com/ivws_api/informedvisapi/"
+from . import config
+
+
+USPS_API_URL = "https://services.usps.com"
+USPS_SERVICE_API_BASE = "https://iv.usps.com/ivws_api/informedvisapi/"
 USPS_ADDRESS_API_URL = 'https://secure.shippingapis.com/ShippingAPI.dll'
 
 headers = {'Content-type': 'application/json'}
@@ -18,17 +20,22 @@ headers = {'Content-type': 'application/json'}
 redis_client = aioredis.Redis(host=config.REDIS_HOST, port=6379, db=0)
 httpx_client = httpx.AsyncClient(timeout=15)
 
+
 async def generate_token_usps(username: str,
-                   passwd: str):
+                              passwd: str):
     data = {
-    "username": username, 
-    "password": passwd, 
-    "grant_type": "authorization", 
-    "response_type": "token", 
-    "scope": "user.info.ereg,iv1.apis", 
-    "client_id": "687b8a36-db61-42f7-83f7-11c79bf7785e"}
-    response = await httpx_client.post(urljoin(USPS_API_URL, "oauth/authenticate"), json=data, headers=headers)
+        "username": username,
+        "password": passwd,
+        "grant_type": "authorization",
+        "response_type": "token",
+        "scope": "user.info.ereg,iv1.apis",
+        "client_id": "687b8a36-db61-42f7-83f7-11c79bf7785e"}
+    try:
+        response = await httpx_client.post(urljoin(USPS_API_URL, "oauth/authenticate"), json=data, headers=headers)
+    except httpx.HTTPError as err:
+        return {"error": "HTTPError", "error_description": str(err)}
     return response.json()
+
 
 async def refresh_token_usps(refresh_token: str):
     data = {
@@ -37,8 +44,12 @@ async def refresh_token_usps(refresh_token: str):
         "response_type": "token",
         "scope": "user.info.ereg,iv1.apis"
     }
-    response = await httpx_client.post(urljoin(USPS_API_URL, "oauth/token"), json=data, headers=headers)
+    try:
+        response = await httpx_client.post(urljoin(USPS_API_URL, "oauth/token"), json=data, headers=headers)
+    except httpx.HTTPError as err:
+        return {"error": "HTTPError", "error_description": str(err)}
     return response.json()
+
 
 async def token_maintain():
     access_token = await redis_client.get("usps_access_token")
@@ -46,9 +57,12 @@ async def token_maintain():
     refresh_token = await redis_client.get("usps_refresh_token")
     now = datetime.datetime.now()
     if next_refresh_time is not None:
-        next_refresh_time = datetime.datetime.fromtimestamp(float(next_refresh_time.decode('utf-8')))
+        next_refresh_time = datetime.datetime.fromtimestamp(
+            float(next_refresh_time.decode('utf-8')))
     if next_refresh_time is None or now > next_refresh_time:
         resp = await generate_token_usps(config.BSG_USERNAME, config.BSG_PASSWD)
+        if "error" in resp:
+            return
         token_type = resp['token_type']
         access_token = resp['access_token']
         refresh_token = resp['refresh_token']
@@ -61,26 +75,39 @@ async def token_maintain():
     else:
         refresh_token = refresh_token.decode('utf-8')
         resp = await refresh_token_usps(refresh_token)
+        if "error " in resp:
+            return
+        token_type = resp['token_type']
+        access_token = resp['access_token']
         expires_in = int(resp['expires_in'])
+        await redis_client.set("usps_access_token", access_token)
+        await redis_client.set("usps_token_type", token_type)
         await redis_client.set("usps_token_nextrefresh", time.time() + expires_in/2.0)
+
 
 async def get_authorization_header():
     next_refresh_time = await redis_client.get("usps_token_nextrefresh")
     if next_refresh_time is not None:
-        next_refresh_time = datetime.datetime.fromtimestamp(float(next_refresh_time.decode('utf-8')))
+        next_refresh_time = datetime.datetime.fromtimestamp(
+            float(next_refresh_time.decode('utf-8')))
     now = datetime.datetime.now()
     if next_refresh_time is None or now > next_refresh_time:
         await token_maintain()
     access_token = (await redis_client.get("usps_access_token")).decode('utf-8')
-    token_type =  (await redis_client.get("usps_token_type")).decode('utf-8')
+    token_type = (await redis_client.get("usps_token_type")).decode('utf-8')
     headers_local = dict()
     headers_local["Authorization"] = token_type + " " + access_token
     return headers_local
 
+
 async def get_piece_tracking(imb: str):
     url = urljoin(USPS_SERVICE_API_BASE, "api/mt/get/piece/imb/" + imb)
-    response = await httpx_client.get(url, headers=await get_authorization_header())
+    try:
+        response = await httpx_client.get(url, headers=await get_authorization_header())
+    except httpx.HTTPError as err:
+        return {"error": "HTTPError", "error_description": str(err)}
     return response.json()
+
 
 async def get_USPS_standardized_address(address):
     req = ""
@@ -97,7 +124,7 @@ async def get_USPS_standardized_address(address):
         req += f"<Zip4>{address['zip4']}</Zip4>"
     else:
         req += "<Zip4/>"
-    
+
     address_xml = f"""
     <Address ID="0">{req}</Address>
     """
